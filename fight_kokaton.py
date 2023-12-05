ec5e5b76e1f886f620e4d69c8aa38bfcd9a8fcc8
takizawa <c0b22091d4@edu.teu.ac.jp>
2023-12-05 14:07:27 +0900

    練習1　Beamクラスの追加

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -6,8 +6,8 @@ import time
 import pygame as pg
 
 
-WIDTH = 1600  # ゲームウィンドウの幅
-HEIGHT = 900  # ゲームウィンドウの高さ
+WIDTH = 1200  # ゲームウィンドウの幅
+HEIGHT = 600  # ゲームウィンドウの高さ
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
 
@@ -122,6 +122,25 @@ class Bomb:
         screen.blit(self.img, self.rct)
 
 
+class Beam:
+    """
+    ビームの実装
+    """
+    def __init__(self, bird: Bird):  # 練習1
+            self.img = pg.image.load(f"{MAIN_DIR}/fig/beam.png")
+            self.rct = self.img.get_rect()
+            self.rct.center = bird.rct.center  # こうかとんの中心座標を取得
+            self.vx, self.vy = +5, 0
+
+    def update(self, screen: pg.Surface):  # 練習1
+        """
+        ビームを速度ベクトルself.vx, self.vyに基づき移動させる
+        引数 screen：画面Surface
+        """
+        self.rct.move_ip(self.vx, self.vy)
+        screen.blit(self.img, self.rct)
+
+
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))    
@@ -135,6 +154,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:  # スペースキーが押されたら
+                beam = Beam(bird)  # ビームインスタンスの生成
         
         screen.blit(bg_img, [0, 0])
         
@@ -148,6 +169,7 @@ def main():
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         bomb.update(screen)
+        beam.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
